f8f533643d14e3224ff5a2eb2eee2bc39f0834b6
chentuo <chentuo@soonsolid.com>
2024-05-14 16:45:34 +0800

    add first cube

---
+++ htmlAndCssReference/src/main.js
@@ -0,0 +1,51 @@
+// import {
+//     BoxBufferGeometry,
+//     Color,
+//     Mesh,
+//     MeshBasicMaterial,
+//     PerspectiveCamera,
+//     Scene,
+//     WebGLRenderer,
+//   } from 'three';
+import {
+    BoxBufferGeometry,
+    Color,
+    Mesh,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    Scene,
+    WebGLRenderer,
+  } from "https://cdn.skypack.dev/three@0.132.2";
+  
+  // just waiting for your beautiful creations!
+  const container = document.querySelector('#scene-container');
+  const scene = new Scene();
+  
+  scene.background = new Color('skyblue');
+  
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 100;
+  
+  const camera = new PerspectiveCamera(fov, aspect, near, far);
+  
+  camera.position.set(0, 0, 10);
+  
+  const geometry = new BoxBufferGeometry(2, 2, 2);
+  
+  const material = new MeshBasicMaterial();
+  
+  const cube = new Mesh(geometry, material);
+  
+  scene.add(cube);
+  
+  const renderer = new WebGLRenderer();
+  
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  
+  renderer.setPixelRatio(window.devicePixelRatio);
+  
+  container.append(renderer.domElement);
+  
+  renderer.render(scene, camera);
