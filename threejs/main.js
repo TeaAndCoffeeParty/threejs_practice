2b4b8dc3180c9fbaeff70266c50c4bc6bbe3fd13
Tuo Chen <chentuo@sprintray.cn>
2024-06-11 16:26:28 +0800

    add split

--- threejs/main.js
+++ threejs/main.js
@@ -80,4 +80,25 @@ function main() {
 
 main()
 
+/* global Split */
 
+// This code is only related to handling the split.
+// Our three.js code has not changed
+Split( [ '#view', '#controls' ], { // eslint-disable-line new-cap
+	sizes: [ 75, 25 ],
+	minSize: 100,
+	elementStyle: ( dimension, size, gutterSize ) => {
+
+		return {
+			'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+		};
+
+	},
+	gutterStyle: ( dimension, gutterSize ) => {
+
+		return {
+			'flex-basis': `${gutterSize}px`,
+		};
+
+	},
+} );
