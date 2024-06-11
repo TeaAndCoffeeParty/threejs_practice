27849262e4eafdf0e79c17fc879537c9555720b9
Tuo Chen <chentuo@sprintray.cn>
2024-06-11 14:06:49 +0800

    the cube rotation

--- threejs/main.js
+++ threejs/main.js
@@ -1,5 +1,16 @@
 import * as THREE from 'three';
 
+function render(time, cube, scene, camera, renderer) {
+	time = performance.now() * 0.001;
+
+	cube.rotation.x = time;
+	cube.rotation.y = time;
+
+	renderer.render(scene, camera);
+
+	requestAnimationFrame(() => render(time, cube, scene, camera, renderer));
+}
+
 function main() {
 	const canvas = document.querySelector('#c');
 	const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
@@ -23,8 +34,9 @@ function main() {
 
 	scene.add(cube);
 
-	renderer.render(scene, camera);
+	requestAnimationFrame(() => render(null, cube, scene, camera, renderer));
 }
 
 main()
+
 
